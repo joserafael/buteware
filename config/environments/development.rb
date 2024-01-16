3d8b9c727bd39d133f5bd9ff497977fa11744794
“Jose <joserafael@outlook.com>
2024-01-16 07:55:03 -0300

    Install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # This will prevent devise from using flash messages which are a default feature and are not present in Rails api mode.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
